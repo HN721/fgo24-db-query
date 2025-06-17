250f18d6c310dc936ad7f3c25636212e141191d0
HN721 <hosea1422@gmail.com>
2025-06-17 10:04:11 +0700

    feat: find movie between rating 5 and 7 and sorting

--- challenge1.sql
+++ challenge1.sql
@@ -1,3 +1,10 @@
 SELECT * FROM movies WHERE year > 2000;
 
 SELECT * from actors WHERE last_name ilike '%s' LIMIT 10;
+
+SELECT *
+FROM movies
+WHERE
+    rankscore BETWEEN 5 AND 7
+    and year BETWEEN 2004 AND 2006
+ORDER BY rankscore DESC
